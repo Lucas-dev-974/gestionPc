65a8aaca58b0dab8f0ae30f439ad7aadf8a609a0
lucas lvn <lucas.lvn97439@gmail.com>
2020-10-21 23:25:36 +0200

    Ajout d'une route

--- Controller/_route.php
+++ Controller/_route.php
@@ -12,4 +12,8 @@ $router->map('GET|POST', '/deconnexion', function(){ // Si on dois effectuer une
     exit();
 });
 
+$router->map('GET|POST', '/del_Pc/i:pc_id', function($pc_id){ // Si on dois effectuer une action et non rediriger ver une autre page alors on lance une fonction dans le mapping
+    header('Location: /connexion');
+    exit();
+});
 $match = $router->match();
