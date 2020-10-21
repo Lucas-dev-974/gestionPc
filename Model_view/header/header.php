f90ee35106403fa9e6a6da0f4be647388014698a
lucas lvn <lucas.lvn97439@gmail.com>
2020-10-21 23:26:12 +0200

    Ajout d'un cdn bootstrap

--- Model_view/header/header.php
+++ Model_view/header/header.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="lib/datepicker/bootstrap-datepicker.css">
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>    
     <script src="lib/datepicker/bootstrap-datepicker.js"></script>
     
